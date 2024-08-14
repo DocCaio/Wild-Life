c298035cd2a958f797470d3ccb07ee616f8a8a20
Caio <108128119+DocCaio@users.noreply.github.com>
2024-08-14 18:55:43 -0300

    up

---
+++ src/app/components/carousel/carouseu2.tsx
@@ -0,0 +1,32 @@
+
+import styles from "../../page.module.css";
+import Image from "next/image";
+import Moosen from '../../../assets/moose.jpg';
+
+export default function Home() {
+    return (
+      <>
+
+
+<div className={styles.item}>
+<Image alt="Wild animal back-ground" src={Moosen} />
+<div className={styles.content}>
+  <div className={styles.author}></div>
+  <div className={styles.title}></div>
+  <div className={styles.topic}></div>
+  <div className={styles.description}>
+    Description
+  </div>
+  <div className={styles.buttons}>
+    <button>See more</button>
+    <button>Subscribe</button>
+
+  </div>
+
+</div>
+</div>
+
+</>
+  );
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,40 @@
 import styles from "./page.module.css";
 import Navbar from "./components/navbar/navbar";
 import Image from "next/image";
-import Lion from '../assets/lion-water.jpg';
+import Lion from '../assets/img1.jpg';
 
 export default function Home() {
   return (
-   <>
-   <Navbar/>
-    <main className={styles.main}>
-    <div className={styles.carousel}>
-      <div className={styles.list}>
-        <div className={styles.item}>
-         <Image alt="Wild animal back-ground" src={Lion}/>
-           <div className={styles.content}>
-            <div className={styles.author}></div>
-            <div className={styles.title}></div>
-            <div className={styles.topic}></div>
-            <div className={styles.description}></div>
+    <>
+      <Navbar />
+      <main className={styles.main}>
+        <div className={styles.carousel}>
+          <div className={styles.list}>
+            <div className={styles.item}>
+              <Image alt="Wild animal back-ground" src={Lion} />
+              <div className={styles.content}>
+                <div className={styles.author}></div>
+                <div className={styles.title}></div>
+                <div className={styles.topic}></div>
+                <div className={styles.description}>
+                  Description
+                </div>
+                <div className={styles.buttons}>
+                  <button>See more</button>
+                  <button>Subscribe</button>
 
-           </div>
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+
         </div>
 
-      </div>
 
-    </div>
-      
-       
-    </main>
+      </main>
     </>
   );
-  
+
 }
