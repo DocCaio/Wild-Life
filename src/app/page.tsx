2532162647c3658b426036914be81cd23b2a9dcd
Caio <108128119+DocCaio@users.noreply.github.com>
2024-08-14 18:28:26 -0300

    uo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
 
 import styles from "./page.module.css";
 import Navbar from "./components/navbar/navbar";
+import Image from "next/image";
+import Lion from '../assets/lion-water.jpg';
 
 export default function Home() {
   return (
    <>
    <Navbar/>
     <main className={styles.main}>
-    
+    <div className={styles.carousel}>
+      <div className={styles.list}>
+        <div className={styles.item}>
+         <Image alt="Wild animal back-ground" src={Lion}/>
+           <div className={styles.content}>
+            <div className={styles.author}></div>
+            <div className={styles.title}></div>
+            <div className={styles.topic}></div>
+            <div className={styles.description}></div>
+
+           </div>
+        </div>
+
+      </div>
+
+    </div>
       
        
     </main>
