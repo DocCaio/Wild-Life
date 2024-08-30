bc6e42def5dc9d3badc47297c31b59206c4fbe01
Caio <108128119+DocCaio@users.noreply.github.com>
2024-08-30 17:50:28 -0300

    up

--- src/app/components/thumbnail/thumbnail.tsx
+++ src/app/components/thumbnail/thumbnail.tsx
@@ -1,57 +1,29 @@
 import styles from './thumbnail.module.css';
 import Image from 'next/image';
-import Lizard from  '../../../assets/lizard.jpeg';
+import Lizard from '../../../assets/lizard.jpeg';
 import Alce from '../../../assets/moose.jpeg';
 import Elefante from '../../../assets/elefante.jpeg';
 import Tucano from '../../../assets/tucano.jpeg';
 
+const items = [
+  { src: Lizard, title: 'Name Slider', description: 'Description' },
+  { src: Alce, title: 'Name Slider', description: 'Description' },
+  { src: Elefante, title: 'Name Slider', description: 'Description' },
+  { src: Tucano, title: 'Name Slider', description: 'Description' },
+];
+
 export default function Thumbnail() {
   return (
     <section className={styles.thumbnail}>
-    <div className={styles.item}>
-        <Image width={0} height={0}  className={styles.img} alt='slide'  src={Lizard}/>
-        <div className={styles.content}>
-            <div className={styles.title}>
-                Name Slider
-            </div>
-            <div className={styles.description}>
-                Description
-            </div>
+      {items.map((item, index) => (
+        <div key={index} className={styles.item}>
+          <Image width={0} height={0} className={styles.img} alt='slide' src={item.src} />
+          <div className={styles.content}>
+            <div className={styles.title}>{item.title}</div>
+            <div className={styles.description}>{item.description}</div>
+          </div>
         </div>
-    </div>
-    <div className={styles.item}>
-        <Image width={0} height={0} className={styles.img} alt='slide' src={Alce}/>
-        <div className={styles.content}>
-            <div className={styles.title}>
-                Name Slider
-            </div>
-            <div className={styles.description}>
-                Description
-            </div>
-        </div>
-    </div>
-    <div className={styles.item}>
-        <Image width={0} height={0}  className={styles.img} alt='slide'  src={Elefante}/>
-        <div className={styles.content}>
-            <div className={styles.title}>
-                Name Slider
-            </div>
-            <div className={styles.description}>
-                Description
-            </div>
-        </div>
-    </div>
-    <div className={styles.item}>
-        <Image width={0} height={0}  className={styles.img} alt='slide' src={Tucano}/>
-        <div className={styles.content}>
-            <div className={styles.title}>
-                Name Slider
-            </div>
-            <div className={styles.description}>
-                Description
-            </div>
-        </div>
-    </div>
-</section>
+      ))}
+    </section>
   );
 }
