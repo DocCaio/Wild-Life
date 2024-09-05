800cf563c96c32f9150e0eb1c214979755bbfbde
Caio <108128119+DocCaio@users.noreply.github.com>
2024-09-05 14:56:53 -0300

    up

--- src/components/Shows/index.tsx
+++ src/components/Shows/index.tsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
+import styles from "./shows.module.css";
 
 
 
 export default function Shows() {
   return (
-<section>
-    <span>Upcoming events</span>
-    <h2>Join us!</h2>
-    <Link to={'/'}>Find out more</Link>
+<section className={styles.container}>
+
+  <div>
+    <span className={styles.title}>Upcoming events</span>
+    <h2 className={styles.text}>Join us!</h2>
+    </div>
+
+    <div>
+      <img src="" alt=""/>
+      <p></p>
+      <div>
+
+      </div>
+    </div>
+
+    <Link className={styles.container__link} to={'/'}>Find out more</Link>
 </section>
   );
 }
