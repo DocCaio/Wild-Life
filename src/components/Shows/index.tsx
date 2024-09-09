103eea866d39cbd0b530603b4a6320263b2d960b
Caio <108128119+DocCaio@users.noreply.github.com>
2024-09-09 14:18:33 -0300

    up

--- src/components/Shows/index.tsx
+++ src/components/Shows/index.tsx
@@ -1,26 +1,31 @@
-import { Link } from "react-router-dom";
-import styles from "./shows.module.css";
-
-
+import { Link } from 'react-router-dom';
+import styles from './shows.module.css';
+import eventsData from '../../data/shows.json'; // Ajuste o caminho conforme necessário
 
 export default function Shows() {
   return (
-<section className={styles.container}>
-
-  <div>
-    <span className={styles.title}>Upcoming events</span>
-    <h2 className={styles.text}>Join us!</h2>
-    </div>
-
-    <div>
-      <img src="" alt=""/>
-      <p></p>
+    <section className={styles.container}>
       <div>
-
+        <span className={styles.title}>Upcoming events</span>
+        <h2 className={styles.text}>Join us!</h2>
       </div>
-    </div>
 
-    <Link className={styles.container__link} to={'/'}>Find out more</Link>
-</section>
+      <div className={styles.cardsContainer}>
+        {eventsData.map((event) => (
+          <div key={event.id} className={styles.cards}>
+            <img src={event.image} alt={`Event ${event.id}`} />
+            <span>{event.description}</span>
+            <span>{event.date}<i className="bi bi-calendar3"></i></span>
+            <span>
+              <i className="bi bi-caret-right-fill"></i>
+            </span>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <Link className={styles.container__link} to={'/'}>Find out more</Link>
+      </div>
+    </section>
   );
 }
