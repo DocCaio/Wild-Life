b6253378ef0f98fda295cc47b57e56db2e9ac4e9
Caio <108128119+DocCaio@users.noreply.github.com>
2024-09-17 15:03:25 -0300

    ro

---
+++ src/components/HomeKart/index.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter , Route , Switch , Redirect } from "react-router-dom";
+
+
+export default function Navbar() {
+  return (
+ <main>
+   <BrowserRouter>
+   <Switch>
+    </Switch>
+    </BrowserRouter>
+ </main>
+  );
+}
